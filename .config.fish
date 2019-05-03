5204e195e94871cc830f60ad9b6e5ddc2f7d18e9
dd <dqdinh@gmail.com>
2019-05-02 20:54:12 -0700

    Update .config.fish

--- .config.fish
+++ .config.fish
@@ -1,12 +1,13 @@
 # Various aliases
 alias ll "ls -lahtr"
 rvm default
-set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" "~/.local/bin" $fish_user_paths
 alias mysql "/usr/local/mysql/bin/mysql"
 set -x EDITOR vim
 set -x TERM xterm-256color
-alias java11 "/Users/ddinh/workspace/java11/Contents/Home/bin/java"
-alias jshell11 "/Users/ddinh/workspace/java11/Contents/Home/bin/jshell"
+
+alias openjava11 "~/workspace/openjava11/Contents/Home/bin/java"
+alias openjshell11 "~/workspace/openjava11/Contents/Home/bin/jshell"
 
 function nvm
   bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
