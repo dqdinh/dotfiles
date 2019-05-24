72b7fa386a14fd33a43099e74ebb2f9e1ced1efd
dd <dqdinh@gmail.com>
2019-05-24 09:04:16 -0700

    Update .config.fish

--- .config.fish
+++ .config.fish
@@ -1,14 +1,32 @@
-# Various aliases
 alias ll "ls -lahtr"
+ 
 rvm default
-set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" "~/.local/bin" $fish_user_paths
-alias mysql "/usr/local/mysql/bin/mysql"
-set -x EDITOR vim
-set -x TERM xterm-256color
+ 
+set -U fish_user_paths "/usr/local/opt/imagemagick@6/bin" "/Users/ddinh/.local/bin" $fish_user_paths
 
-alias openjava11 "~/workspace/openjava11/Contents/Home/bin/java"
-alias openjshell11 "~/workspace/openjava11/Contents/Home/bin/jshell"
+ set -U EDITOR vim
+ set -U TERM xterm-256color
 
-function nvm
-  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
+ alias mysql "/usr/local/mysql/bin/mysql"
+
+ set JAVA_8_HOME (/usr/libexec/java_home -v1.8)
+ set JAVA_11_HOME (/usr/libexec/java_home -v11)
+
+ alias java8 'export JAVA_HOME=$JAVA_8_HOME'
+ alias jshell8 '/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin/jshell'
+
+ alias java11 'export JAVA_HOME=$JAVA_11_HOME'
+ alias jshell11 '/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/jshell'
+
+ # default to Java 8
+ # https://stackoverflow.com/a/52524114
+ java8
+
+ # Then to change versions, just use the alias.
+
+ # java11
+ # java -version
+
+ function nvm
+   bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+ end
